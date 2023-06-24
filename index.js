668cd39a14662a4fec8e5298b8e2140091f72d6d
BuildTools <unconfigured@null.spigotmc.org>
2023-06-24 23:11:29 +0200

    commit3

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// JavaScript to close the menu when a link is clicked
 document.addEventListener("DOMContentLoaded", function() {
     var links = document.querySelectorAll(".menu a");
     var hoverTimeout;
     var isMenuHovered = false;
+    var isMenuOpen = false; // Track menu open state
   
     // Function to handle menu activation
     function activateMenu() {
       if (isMenuHovered) {
+        isMenuOpen = true; // Set menu open state
+        document.querySelector(".nav").style.maxHeight = "100%"; // Open the menu
       }
     }
   
@@ -24,14 +26,27 @@ document.addEventListener("DOMContentLoaded", function() {
       clearTimeout(hoverTimeout);
     });
   
+    // Add click event listener to the hamburger menu
+    document.querySelector(".hamb").addEventListener("click", function() {
+      if (isMenuOpen) {
+        isMenuOpen = false; // Set menu open state
+        document.querySelector(".nav").style.maxHeight = "0"; // Close the menu
+      } else {
+        isMenuOpen = true; // Set menu open state
+        document.querySelector(".nav").style.maxHeight = "100%"; // Open the menu
+      }
+    });
+  
     // Add click event listeners to the navigation links
     links.forEach(function(link) {
       link.addEventListener("click", function() {
+        isMenuOpen = false; // Set menu open state to false when a link is clicked
+        document.querySelector(".nav").style.maxHeight = "0"; // Close the menu
       });
     });
   });
-  
-// JavaScript to handle the typing animation
+
+  // JavaScript to handle the typing animation
 document.addEventListener("DOMContentLoaded", function() {
     var text = "LandRP.net"; // Text to animate
     var typingCursor = document.getElementById("typing-animation");
